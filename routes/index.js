63bf2db256fe44d694a4c2691736ca572052e033
VRamazing <r.vignesh149@gmail.com>
2017-07-24 17:43:46 +0530

    quotes logic added.Working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var quotes = require('../lib/quotes');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Random Quote Generator' });
+  quote = quotes.getQuote();
+  res.render('index', { title: 'Random Quote Generator' , name:quote.name, text:quote.text});
 });
 
 module.exports = router;
