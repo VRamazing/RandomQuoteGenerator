24781e17f9db8a0bef71f1d6790b8721e6cb5ed6
VRamazing <r.vignesh149@gmail.com>
2017-07-24 18:26:54 +0530

    Color api created and background colors automatically updated

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var quotes = require('../lib/quotes');
 var router = express.Router();
-
+var colors=require('../lib/colors');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   quote = quotes.getQuote();
-  res.render('index', { title: 'Random Quote Generator' , name:quote.name, text:quote.text});
+  color = colors.getColor();
+  res.render('index', { title: 'Random Quote Generator' , name:quote.name, text:quote.text, color:color.name});
 });
+
+router.get('/quote',function(req,res,next){
+	quote = quotes.getQuote();
+	res.send(quote);
+})
+
+router.get('/color',function(req,res,next){
+	color = colors.getColor();
+	res.send(color);
+})
 
 module.exports = router;
